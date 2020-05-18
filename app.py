f179f56424eb5791338e9da4b87e62ef385a5159
GrAMATO <61666789+GrAMATO@users.noreply.github.com>
2020-05-18 12:09:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
 
 ########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)
 server = app.server
 app.title=tabtitle
 
